b7598a8842a73611d28c67d5a59cab65a9e1623e
Kalpani Ekanayake <ekanayake.emkn@gmail.com>
2026-03-26 20:44:54 +0530

    fix: configure API base URL using config file

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,6 +1,8 @@
 import { Meal, Ingredient, PlannerEntry, Notification, ShoppingItem, User } from './types';
 
-const API_BASE_URL = "https://6c905a3b-3aaf-44b8-9641-4c2e8a899abb-dev.e1-us-east-azure.choreoapis.dev/crave-meal-planner/backend/v1.0";
+import config from "../config";
+
+const API_BASE_URL = config.API_BASE_URL;
 
 async function fetcher<T>(url: string, options?: RequestInit): Promise<T> {
   const token = localStorage.getItem('auth_token');
